4e9be107c40946ce30522fe653b9f67f78e90439
Balys Valentukevicius <balys.v@gmail.com>
2014-04-23 12:01:02 +0300

    Added a couple more screen changes to FlowTest

--- app/src/androidTest/java/co/lemonlabs/mortar/tests/FlowTest.java
+++ app/src/androidTest/java/co/lemonlabs/mortar/tests/FlowTest.java
@@ -2,13 +2,19 @@ package co.lemonlabs.mortar.tests;
 
 import android.test.suitebuilder.annotation.SmallTest;
 
+import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
+
 import co.lemonlabs.mortar.example.R;
 
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withContentDescription;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsAnything.anything;
 
 @SmallTest
 public class FlowTest extends BaseTest {
@@ -27,6 +33,21 @@ public class FlowTest extends BaseTest {
 
         onView(withId(R.id.stub_text)).check(matches(withText("STUB SCREEN 420")));
 
+        tag = "2. Click Action Bar back should return to Gallery";
+        onView(withId(android.R.id.home)).perform(click());
+        screenshot(tag);
+
+        onView(withId(R.id.gallery_progress)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+
+        tag = "3. Click on Action Bar Toggle should open Navigation Drawer";
+        onView(withId(android.R.id.home)).perform(click());
+        screenshot(tag);
+
+        tag = "4. Child on second element in Drawer should go to Child Presenter Example Screen";
+        onData(anything()).inAdapterView(withContentDescription("drawerList")).atPosition(1).perform(click());
+        screenshot(tag);
     }
+
+
 
 }
