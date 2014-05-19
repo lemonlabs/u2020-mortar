3fc607d76c36720edd5aab7dde986baf95c53915
Balys Valentukevicius <balys.v@gmail.com>
2014-05-19 21:16:32 +0300

    Documentation for Transition annotation

--- app/src/main/java/co/lemonlabs/mortar/example/core/anim/Transition.java
+++ app/src/main/java/co/lemonlabs/mortar/example/core/anim/Transition.java
@@ -6,6 +6,17 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * Accepts 4 Animator resource ids corresponding:
+ * <ol>
+ * <li> View to be added</li>
+ * <li> View to be removed</li>
+ * <li>  View to be restored when popping the backstack</li>
+ * <li> View to be popped from the backstack</li>
+ * </ol>
+ *
+ * If no animation should be run, use an empty animator:  {@link R.animator.empty}
+ */
 @Retention(RUNTIME) @Target(TYPE)
 public @interface Transition {
     int[] value();
